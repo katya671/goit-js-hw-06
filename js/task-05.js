360e65d45bd5e5243ec1da078198a52e71eb5d37
Толочко Катерина <bantiktokinpakistan@gmail.com>
2023-06-03 17:28:05 +0300

    Update task-05.js

--- js/task-05.js
+++ js/task-05.js
@@ -4,7 +4,7 @@ const output = document.querySelector('#name-output');
 input.addEventListener('input', (event) => {
     output.textContent = event.currentTarget.value.trim();
     
-    if (event.currentTarget.value === '') {
+    if (event.currentTarget.value.trim() === '') {
         output.textContent = 'Anonymous';
     }
 });
