f222e82a94d0b4c785b4970b07c893ce37d2a05d
Толочко Катерина <bantiktokinpakistan@gmail.com>
2023-06-03 17:26:04 +0300

    Update task-08.js

--- js/task-08.js
+++ js/task-08.js
@@ -2,13 +2,13 @@ const form = document.querySelector('.login-form');
 
 const handleSubmit = (event) => {
     event.preventDefault();
-    const emailValue = form.elements.email.value;
-    const passwordValue = form.elements.password.value;
-    if (emailValue === '' || passwordValue === '') {
+    const { elements: { email, password }} = event.currentTarget;
+
+    if (email.value === '' || password.value === '') {
         alert('Всі поля повинні бути заповнені.')
     }
     else {
-        console.log({email: emailValue, password: passwordValue})
+        console.log({email: email.value, password: password.value})
     }
     form.reset();
 }
