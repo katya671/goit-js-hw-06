a5f19cf51c21f4a06b7958d736f1a0d878341b00
Толочко Катерина <bantiktokinpakistan@gmail.com>
2023-06-05 17:11:22 +0300

    Fix form.reset()

--- js/task-08.js
+++ js/task-08.js
@@ -1,17 +1,17 @@
-const form = document.querySelector('.login-form');
+const form = document.querySelector(".login-form");
 
 const handleSubmit = (event) => {
-    event.preventDefault();
-    const { elements: { email, password }} = event.currentTarget;
+  event.preventDefault();
+  const {
+    elements: { email, password },
+  } = event.currentTarget;
 
-    if (email.value === '' || password.value === '') {
-        alert('Всі поля повинні бути заповнені.')
-    }
-    else {
-        console.log({email: email.value, password: password.value})
-    }
+  if (email.value === "" || password.value === "") {
+    alert("Всі поля повинні бути заповнені.");
+  } else {
+    console.log({ email: email.value, password: password.value });
     form.reset();
-}
+  }
+};
 
-form.addEventListener('submit', handleSubmit);
-
+form.addEventListener("submit", handleSubmit);
